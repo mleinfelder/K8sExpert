b3129c69927351c7830c459c4b21ad97c5934500
root <root@kindlab>
2023-10-18 00:40:23 +0000

    install

---
+++ installall.sh
@@ -0,0 +1,11 @@
+#/bin/bash!
+
+curl -Lo ./kind https://kind.sigs.k8s.io/dl/v0.20.0/kind-linux-amd64
+chmod +x ./kind
+sudo mv ./kind /usr/local/bin/kind
+
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add
+sudo apt-add-repository "deb http://apt.kubernetes.io/ kubernetes-xenial main"
+apt-get install kubeadm kubelet kubectl
+apt-mark hold kubeadm kubelet kubectl
+kubeadm version
